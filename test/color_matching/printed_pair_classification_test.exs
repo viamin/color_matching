64cab4a9ab8e0242bc7576a3b1ae375bff1c8bf9
Paid Agent <paid-agents@paid-agents.com>
2026-07-30 02:41:36 +0000

    fix: align printed pair classification active default test

--- test/color_matching/printed_pair_classification_test.exs
+++ test/color_matching/printed_pair_classification_test.exs
@@ -34,7 +34,17 @@ defmodule ColorMatching.Persistence.PrintedPairClassificationTest do
     assert changeset.changes.active == false
   end
 
-  test "requires identifying fields and active state" do
+  test "defaults active when attrs omit it" do
+    attrs = Map.delete(@valid_attrs, :active)
+
+    changeset = PrintedPairClassification.changeset(%PrintedPairClassification{}, attrs)
+
+    assert changeset.valid?
+    assert changeset.data.active == true
+    refute Map.has_key?(changeset.changes, :active)
+  end
+
+  test "requires identifying fields" do
     changeset = PrintedPairClassification.changeset(%PrintedPairClassification{}, %{})
 
     refute changeset.valid?
@@ -43,8 +53,7 @@ defmodule ColorMatching.Persistence.PrintedPairClassificationTest do
              test_sheet_pair_id: ["can't be blank"],
              reproduction_profile_id: ["can't be blank"],
              illuminant: ["can't be blank"],
-             classification: ["can't be blank"],
-             active: ["can't be blank"]
+             classification: ["can't be blank"]
            } = errors_on(changeset)
   end
 
